b6205e3b7187aa9ac90ab7dd8b147566ceccc67a
Nikola Vuletić <nikolavuletic412@gmail.com>
2025-06-19 23:44:25 +0200

    Header component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import {React} from "react"
+import Header from "./components/header/header.jsx"
 
 function App() {
   
-
   return (
-    <h1>My to do app</h1>
+    <Header />
   )
 }
 
---
+++ src/components/header/header.jsx
@@ -0,0 +1,9 @@
+import './header.css';
+
+export default function Header() {
+    return (
+    <div className="div-header" >
+        <h1 className="header" >👜 To-Do App 🧰</h1>
+    </div>
+    )
+};
